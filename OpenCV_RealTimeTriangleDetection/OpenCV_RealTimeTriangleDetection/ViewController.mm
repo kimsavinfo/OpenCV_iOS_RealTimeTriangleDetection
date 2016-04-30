177ab7a58f5776cd416b305819c15294945f50e5
Kim SAVAROCHE <kimsavinfo@gmail.com>
2016-04-30 22:34:20 +0200

    draw all contours

--- OpenCV_RealTimeTriangleDetection/OpenCV_RealTimeTriangleDetection/ViewController.mm
+++ OpenCV_RealTimeTriangleDetection/OpenCV_RealTimeTriangleDetection/ViewController.mm
@@ -44,9 +44,28 @@
 - (void)processImage:(Mat&)image;
 {
     Mat image_copy;
+    Mat canny_output;
+    int thresh = 100;
+    std::vector<std::vector<cv::Point> > contours;
+    std::vector<Vec4i> hierarchy;
+    
+    /// Convert image to gray and blur it
     cvtColor(image, image_copy, CV_BGRA2BGR);
-    bitwise_not(image_copy, image_copy);
-    cvtColor(image_copy, image, CV_BGR2BGRA);
+    blur(image_copy, image_copy, cv::Size(3,3));
+    
+    /// Detect edges using canny
+    Canny( image_copy, canny_output, thresh, thresh*2, 3 );
+    
+    /// Find contours
+    findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+    
+    /// Draw contours
+    // Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( 255, 0, 0 );
+        drawContours( image, contours, i, color, 2, 8, hierarchy, 0, cv::Point() );
+    }
 }
 #endif
 
